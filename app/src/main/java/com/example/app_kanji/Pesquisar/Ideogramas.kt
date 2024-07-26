e146154b9fd89d179c793fca323e5522d3ca6d4a
Julia Hayashi <julia.iwazaki@gmail.com>
2024-07-25 22:53:42 -0300

    Realtime Database

---
+++ app/src/main/java/com/example/app_kanji/Pesquisar/Ideogramas.kt
@@ -0,0 +1,18 @@
+package com.example.app_kanji.Pesquisar
+
+data class Ideogramas(
+    var ex1_significado: String? = null,
+    var ex2_significado: String? = null,
+    var ex3_significado: String? = null,
+    var ex4_significado: String? = null,
+    var exemplo1: String? = null,
+    var exemplo2: String? = null,
+    var exemplo3: String? = null,
+    var exemplo4: String? = null,
+    var frequencia: Int = 0,
+    var imagem: String? = null,
+    var kunyomi: String? = null,
+    var onyomi: String? = null,
+    var qtd_tracos: Int = 0,
+    var significado: String? = null
+)
