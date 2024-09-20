944672bcea3fdb066eeee818157c3097e59d9cf2
Julia Hayashi <julia.iwazaki@gmail.com>
2024-09-20 20:44:25 -0300

    Espaço extra em Fragment Treinar, abaixo das Categorias

--- app/src/main/java/com/example/app_kanji/Treinar/AdapterClass.kt
+++ app/src/main/java/com/example/app_kanji/Treinar/AdapterClass.kt
@@ -8,24 +8,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.app_kanji.R
 
 class AdapterClass(private val dataList: List<DataClass>, private val itemClickListener: OnItemClickListener) :
-    RecyclerView.Adapter<AdapterClass.ViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     interface OnItemClickListener {
         fun onItemClick(title: String)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
-        return ViewHolder(view)
+    companion object {
+        private const val VIEW_TYPE_ITEM = 0
+        private const val VIEW_TYPE_EXTRA_SPACE = 1
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val data = dataList[position]
-        holder.bind(data)
+    override fun getItemViewType(position: Int): Int {
+        return if (position == dataList.size) VIEW_TYPE_EXTRA_SPACE else VIEW_TYPE_ITEM
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ITEM) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.categoria_layout, parent, false)
+            ViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.espaco_vazio_layout, parent, false)
+            EmptySpaceViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder) {
+            val data = dataList[position]
+            holder.bind(data)
+        }
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return dataList.size + 1 // Adiciona 1 para o espaço extra
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
@@ -47,4 +63,6 @@ class AdapterClass(private val dataList: List<DataClass>, private val itemClickL
             }
         }
     }
+
+    inner class EmptySpaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
